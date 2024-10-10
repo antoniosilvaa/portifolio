e01a63a55e94ffeaa98b24453e11c3d3d62d2192
antonio <antg3102@gmail.com>
2024-10-09 22:34:52 -0300

    imolementaçoes

--- portal flamidias/index.js
+++ portal flamidias/index.js
@@ -1,6 +1,7 @@
 let idade = Number(prompt("digite sua idade"))
-if (idade <= 17){
- alert("essa pagina é para maiores de 17 anos")
+if (idade < 18){
+ alert(idade)
+    alert("essa pagina é para maiores de 18 anos")
  window.location.href = "https://https://www.google.com/search?q=error+401&oq=error&gs_lcrp=.com";
 }else{
     alert('Sejá bem vindo a nossa pagina')
